050f084bf1abe32246b1f461c2db6387d8b3c243
alejho4 <alejhotabraj@gmail.com>
2021-08-25 00:58:01 +0000

    lunes

--- src/components/pages/Yo.js
+++ src/components/pages/Yo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Yo() {
   return (
     <section className={'yo'}>
-      <h1>"Desarrollador  <span>Full Stack</span> con orientación <span>al backend</span> y los datos "</h1>
+      <h1>"Dev  <span>Full Stack</span> con orientación <span>al backend</span> y los datos "</h1>
     </section>
   );
 }
